b86f0002658fe1a79fbefa42eea21e610c470176
Anastasia Shatokhina <shnatya@gmail.com>
2021-07-07 23:44:13 -0700

    Completed

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(x, y) {
+    return (x+y);
+}
+
+function subtract(x, y) {
+    return (x-y);
+}
+
+function multiply(x, y) {
+    return (x*y);
+}
+
+function divide(x, y) {
+    return (x/y);
+}
+
+function increment(x) {
+    return (x += 1);
+}
+
+function decrement(x) {
+    return (x -= 1);
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
+//console.log(preserveDecimal("7.89"))
